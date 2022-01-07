6b48f46ce52709a67abe2364dd02d40291c2bdca
tamara-snyder <tamara.snyder@yale.edu>
2022-01-07 10:53:35 -0500

    Minor design tweaks

--- script.js
+++ script.js
@@ -17,37 +17,42 @@ class Book {
     createCard() {
         const books = document.querySelector('.books')
         const div = document.createElement('div')
+        const edit = document.createElement('div')
         const title = document.createElement('p')
         const author = document.createElement('p')
         const pages = document.createElement('p')
         const del = document.createElement('button')
         const readContainer = document.createElement('div')
-        let readInput = document.createElement('input')
         const readLabel = document.createElement('label')
+        let readInput = document.createElement('input')
 
         div.classList.add('card')
+        edit.classList.add('edit')
         title.setAttribute('id', 'book-title')
         author.setAttribute('id', 'book-author')
         pages.setAttribute('id', 'book-pages')
         del.classList.add('delete')
-        del.innerText = 'Remove'
         readContainer.classList.add('checkbox')
         readInput.setAttribute('type', 'checkbox')
         readInput.setAttribute('id', 'cb')
         readInput.setAttribute('name', 'cb')
+        readLabel.setAttribute('for', 'have-read')
+        readLabel.setAttribute('id', 'have-read')
 
         books.appendChild(div)
         div.appendChild(title)
         div.appendChild(author)
         div.appendChild(pages)
-        div.appendChild(del)
-        div.appendChild(readContainer)
+        div.appendChild(edit)
+        edit.appendChild(del)
+        edit.appendChild(readContainer)
         readContainer.appendChild(readInput)
         readContainer.appendChild(readLabel)
 
         title.innerText = this.title
         author.innerText = this.author
         pages.innerText = `${this.pages} pages`
+        del.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M16 9v10H8V9h8m-1.5-6h-5l-1 1H5v2h14V4h-3.5l-1-1zM18 7H6v12c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7z"/></svg>'
         readInput.checked = this.isRead ? true : false
         readLabel.innerText = 'Have read'
 
